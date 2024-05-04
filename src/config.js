ec31ecc5d58f19822144552924acb1a49cf87914
SudharsanK58 <sudharsan58@gmail.com>
2024-05-04 22:50:56 +0530

    Reset base url hardware.api.sudharsan.in

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
-// const BASE_URL = "https://hardware.api.sudharsan.in/hardware-api"; // Replace "example.com" with your DNS name
-const BASE_URL = "https://esp32ota.api.sudharsan.in"; // Replace "example.com" with your DNS name
+const BASE_URL = "https://hardware.api.sudharsan.in/hardware-api"; // Replace "example.com" with your DNS name
+// const BASE_URL = "https://esp32ota.api.sudharsan.in"; // Replace "example.com" with your DNS name
 export default BASE_URL;
