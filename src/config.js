4a7bd00d1bec66a2a3c619c919be658082192e61
SudharsanK58 <sudharsan58@gmail.com>
2024-05-11 18:09:20 +0530

    Moved to live url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // config.js
-// const BASE_URL = "https://hardware.api.sudharsan.in/hardware-api"; // Replace "example.com" with your DNS name
+const BASE_URL = "https://hardware.api.sudharsan.in/hardware-api"; // Replace "example.com" with your DNS name
 // const BASE_URL = "https://esp32ota.api.sudharsan.in"; // Replace "example.com" with your DNS name
-const BASE_URL = "http://localhost:8001"; // Replace "example.com" with your DNS name
+// const BASE_URL = "http://localhost:8001"; // Replace "example.com" with your DNS name
 export default BASE_URL;
